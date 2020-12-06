af30f333f1f46fe14d4200d18c73ee8b6f63b39e
Ville Lehtonen <violehtonen@gmail.com>
2020-12-06 13:49:55 +0100

    day 6 script and input

---
+++ exercises/day6.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import sys
+
+
+def count_answers(form_answers):
+  counts = 0
+  
+  for answer in form_answers:
+    counts += len(answer)
+  
+  return counts
+
+
+def find_shared_answers(answer_list):
+  shared_answers = []
+  
+  for answer in answer_list:
+    shared_answer = ""
+    s_answer = "".join(answer)
+    unique_answers = "".join(set("".join(answer)))
+    
+    for ans in unique_answers:
+      ans_count = s_answer.count(ans)
+      if ans_count == len(answer):
+        shared_answer = "".join([shared_answer, ans])
+    
+    shared_answers.append(shared_answer)
+    
+  return shared_answers
+
+
+def find_unique_answers(answer_list):
+  form_answers = []
+  
+  for answer in answer_list:
+    unique_answers = "".join(set("".join(answer)))      
+    form_answers.append(unique_answers)
+    
+  return form_answers
+
+
+def parse_input(args):
+  answer_list = []
+  
+  with open(args, "r") as f:
+    raw_input = f.read().split("\n\n")
+    
+    for line in raw_input:
+      answer = line.split("\n")
+      answer_list.append(answer)
+      
+  return answer_list
+
+
+if __name__ == "__main__":
+  form_answers = parse_input(sys.argv[1])
+  
+  # Part 1: Sum of answer counts
+  unique_answers = find_unique_answers(form_answers)
+  answer_counts = count_answers(unique_answers)
+  print(f"answer to part 1: {answer_counts}")
+
+  # Part 2: Sum of shared answer counts
+  shared_answers = find_shared_answers(form_answers)
+  answer_counts2 = count_answers(shared_answers)
+  print(f"answer to part 2: {answer_counts2}")
